37cd541328b5e6a5457883a7c991605037f63ead
shambhosle1212 <milindgadge23@gmail.com>
2024-07-21 15:08:18 +0530

    pim page is created

---
+++ src/main/java/Pagelayer/PimPage.java
@@ -0,0 +1,33 @@
+package Pagelayer;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import Baselayer.BaseClass;
+
+public class PimPage extends BaseClass {
+	
+	@FindBy(xpath="//span[text()='PIM']")
+	private WebElement pim;
+
+	@FindBy(xpath="//a[text()='Add Employee']")
+	private WebElement addEmploy;
+
+	public PimPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+
+	public void clickonPim()
+	{
+		pim.click();
+	}
+
+	public void clickOnAddEmploy()
+	{
+		addEmploy.click();
+	}
+
+
+}
